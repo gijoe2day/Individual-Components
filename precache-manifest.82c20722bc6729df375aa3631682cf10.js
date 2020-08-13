f82381a8e8ba381410040ffd07155c2115cc344e
gijoe2day <gjuliao32@hotmail.com>
2020-08-13 09:04:45 -0500

    Updates

---
+++ precache-manifest.82c20722bc6729df375aa3631682cf10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293b0eea0a6b6f98091cbc3abfdbb744",
+    "url": "/Individual-Components/index.html"
+  },
+  {
+    "revision": "0f68895b39f08da39fe8",
+    "url": "/Individual-Components/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "68fc412983351f0bcdd6",
+    "url": "/Individual-Components/static/css/main.ead1cf15.chunk.css"
+  },
+  {
+    "revision": "0f68895b39f08da39fe8",
+    "url": "/Individual-Components/static/js/2.cc7fadae.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Individual-Components/static/js/2.cc7fadae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fc412983351f0bcdd6",
+    "url": "/Individual-Components/static/js/main.c3d0bb50.chunk.js"
+  },
+  {
+    "revision": "ab09c696e29e8aeb0a91",
+    "url": "/Individual-Components/static/js/runtime-main.75cc6424.js"
+  },
+  {
+    "revision": "32fad58dd430e6614b8853a5681909a3",
+    "url": "/Individual-Components/static/media/home_react.32fad58d.png"
+  }
+]);
